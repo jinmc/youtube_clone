1409c714b0fd0eda15d9030bbed6554fc5a25b03
jinmc <jinmo55555@gmail.com>
2021-08-24 18:09:39 +0900

    make videodetail not showing up tho

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAA45fZ5triAedogpjRKUb4JXD3gpomZac';
 // import './index.css';
@@ -16,11 +17,12 @@ class App extends Component {
       this.setState({ videos });
     });
   }
-  
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail videos={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
