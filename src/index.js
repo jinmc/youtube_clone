356af7de54cb492dfae954ce2a627604e2044141
jinmc <jinmo55555@gmail.com>
2021-08-24 13:48:31 +0900

    cleanup 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAA45fZ5triAedogpjRKUb4JXD3gpomZac';
 // import './index.css';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    let videos = [];
+    let videoList = YTSearch({key: API_KEY, videos});
+    console.log(videos);
+  }
+
   render() {
     return <h1>hello</h1>;
   }
