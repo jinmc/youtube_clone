4a8b73f97b584d81ff541a16ab317cf5d6202d87
jinmc <jinmo55555@gmail.com>
2021-08-24 17:04:23 +0900

    dynamically changing video length as asynchronously getting video from yt

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAA45fZ5triAedogpjRKUb4JXD3gpomZac';
 // import './index.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    let videos = [];
-    let videoList = YTSearch({key: API_KEY, videos});
-    console.log(videos);
+
+    this.state = { videos: [] };
+    // console.log(this.state)
+    YTSearch({key: API_KEY, term: 'kpop'}, (data) => {
+      console.log(data);
+      // this.state= {videos:videos}
+      console.log(this);
+      // videoList = data;
+      this.setState({ videos: data });
+    });
   }
+
+   /**
+  componentDidMount() {
+    let videoList = null;
+    YTSearch({key: API_KEY, term: 'kpop'}, (data) => {
+      console.log(data);
+      // this.state= {videos:videos}
+      console.log(this);
+      videoList = data;
+      // this.setState({ videos: data });
+    });
+    this.setState({videos: videoList});
+    console.log(`this.state`);
+    // this.state = {videos: videoList};
+    // console.log(videoList);
+  }
+  */
+
 
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
   }
 }
