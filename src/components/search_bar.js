133f85194c78d193c147a359c7b71594fdc42359
jinmc <jinmo55555@gmail.com>
2021-08-24 14:59:56 +0900

    make search bar to use sates

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />;
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term : ''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    render(){
+        // console.log(this.state.term);
+        return (
+            <input
+                value={this.state.term} 
+                onChange={this.onInputChange} 
+            />
+        );
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+        // console.log(event.target.value);
+    }
 }
 
 export default SearchBar;
